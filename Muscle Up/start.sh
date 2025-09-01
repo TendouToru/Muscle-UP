2d51805cb6666600389d4a1205931a74f9cf8a5f
HT <68563075+TendouToru@users.noreply.github.com>
2025-09-01 03:13:16 +0200

    migrate oy

--- Muscle Up/start.sh
+++ Muscle Up/start.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-gunicorn --bind 0.0.0.0:$PORT app:app
+echo "Activating virtual environment..."
+source .venv/bin/activate
+
+echo "Running database migration script..."
+python migrate_db.py
+
+echo "Starting Gunicorn server..."
+gunicorn --bind 0.0.0.0:$PORT run:app
