05ec48ee2937acb34ef8b45bb5156025a3f171f5
HT <68563075+TendouToru@users.noreply.github.com>
2025-09-01 03:44:42 +0200

    ich raste aus

--- Muscle Up/start.sh
+++ Muscle Up/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running database migration..."
-flask db upgrade
+flask db upgrade --tag force_migration
 
 echo "Starting Gunicorn server..."
 gunicorn --bind 0.0.0.0:$PORT app:app
