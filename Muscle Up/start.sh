b0d1e6efbc4f5677b16a9a73289e81f63d6ff493
HT <68563075+TendouToru@users.noreply.github.com>
2025-09-01 03:52:21 +0200

    ig

--- Muscle Up/start.sh
+++ Muscle Up/start.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Running database migration..."
-flask db downgrade base
-
-echo "Cleaning up alembic_version table..."
-# You might need to adjust this command if your alembic_version table name is different
-flask shell <<EOF
-from app import db
-db.engine.execute("DELETE FROM alembic_version;")
-db.session.commit()
-EOF
-
 echo "Forcing database migration..."
-flask db upgrade --tag force_migration
+# This command bypasses the alembic history and runs the upgrade function directly
+alembic upgrade head
 
-# Exit after migration to prevent the server from starting prematurely
-exit 0
+echo "Starting Gunicorn server..."
+gunicorn --bind 0.0.0.0:$PORT app:app
